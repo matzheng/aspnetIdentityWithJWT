e92c89447bcc270fe330d6ebfe8216d999f369f6
matzheng <99284419@qq.com>
2016-08-17 16:23:57 +0800

    chkin

---
+++ AspNetIdentity.WebApi/Services/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SendGrid;
+using Microsoft.AspNet.Identity;
+using System.Net;
+using System.Configuration;
+
+namespace AspNetIdentity.WebApi.Services
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            await configSendGridasync(message);
+        }
+
+        private async Task configSendGridasync(IdentityMessage message)
+        {
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(message.Destination);
+            myMessage.From = new System.Net.Mail.MailAddress("99284419@qq.com", "99284419");
+            myMessage.Subject = message.Subject;
+            myMessage.Text = message.Body;
+            myMessage.Html = message.Body;
+
+            var credentials = new NetworkCredential(ConfigurationManager.AppSettings["emailService:Account"],
+                        ConfigurationManager.AppSettings["emailService:Password"]);
+            var transportweb = new Web(credentials);
+
+            if(transportweb != null)
+            {
+                await transportweb.DeliverAsync(myMessage);
+            }
+            else
+            {
+                await Task.FromResult(0);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#Intruduce
-a sample code copied from `http://bitoftech.net/2015/01/21/asp-net-identity-2-with-asp-net-web-api-2-accounts-management/` which demostrate `asp.net ideneity/owin/JWT`
+#Introduce
+a sample code copied from `http://bitoftech.net/2015/01/21/asp-net-identity-2-with-asp-net-web-api-2-accounts-management/` which demostrate `asp.net identity/owin/JWT`
